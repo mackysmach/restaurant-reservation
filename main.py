ddd1ff713b4877403a2e058c9df1bcea93bc606e
mackysmach <90068336+mackysmach@users.noreply.github.com>
2022-02-06 17:12:11 +0700

    new

--- main.py
+++ main.py
@@ -1,41 +1,173 @@
-from fastapi import FastAPI
+# from json import JSONEncoder
+# from fastapi import FastAPI,HTTPException
+# from pymongo import MongoClient
+# from pydantic import BaseModel
+# from fastapi.encoders import jsonable_encoder
+# class Reservation(BaseModel):
+#     name : str
+#     time: int
+#     table_number: int
+    
+# client = MongoClient('mongodb://localhost', 27017)
+
+# # TODO fill in database name
+# db = client["Mac_restaurant"]
+
+# # TODO fill in collection name
+# collection = db["user"]
+
+# app = FastAPI()
+
+
+# # TODO complete all endpoint.
+# @app.get("/reservation/by-name/{name}")
+# def get_reservation_by_name(name:str):
+#     r=collection.find({"name":name},{"_id":0,"name":1,"time":1,"table_number":1})
+#     my_result=[]
+#     for i in r:
+#         my_result.append(i)
+#     return{
+#         "result":my_result
+#     }
+
+
+   
+# @app.get("/reservation/by-table/{table}")
+# def get_reservation_by_table(table: int):
+#     r=collection.find({"table_number":table},{"_id":0,"name":1,"time":1,"table_number":1})
+#     my_result=[]
+#     for i in r:
+#         my_result.append(i)
+#     return{
+#         "result":my_result
+#     }
+
+# @app.post("/reservation")
+# def reserve(reservation : Reservation):
+#     r=collection.find_one({"time":reservation.time,"table_number":reservation.table_number},{"_id":0})
+#     if(r!=None):
+#         return{
+#             "table is not available"
+#         }
+#     if(reservation.table_number>12):
+#         return{
+#             "table number is not exist"
+#         }
+#     else:
+#         m=jsonable_encoder(reservation)
+#         collection.insert_one(m)
+#         return{
+#             "reservation done"
+#         }
+
+
+
+# # @app.put("/reservation/update/")
+# # def update_reservation(reservation: Reservation):
+# #     pass
+
+# # @app.delete("/reservation/delete/{name}/{table_number}")
+# # def cancel_reservation(name: str, table_number : int):
+# #     pass
+
 from pymongo import MongoClient
+
+from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
+from typing import Optional
+from fastapi.encoders import jsonable_encoder
+
 
 class Reservation(BaseModel):
-    name : str
+    name: str
     time: int
     table_number: int
-    
+
+
 client = MongoClient('mongodb://localhost', 27017)
 
-# TODO fill in database name
-db = client["<put your database name>"]
+db = client["touch_restaurant"]
 
-# TODO fill in collection name
-collection = db["<put your collection name>"]
+collection = db["reservation"]
 
 app = FastAPI()
 
 
-# TODO complete all endpoint.
 @app.get("/reservation/by-name/{name}")
-def get_reservation_by_name(name:str):
-    pass
+def get_reservation_by_name(name: str):
+    list_result = list(collection.find({"name": name}, {"_id": 0}))
+    if len(list_result) != 0:
+        data = []
+        for result in list_result:
+            data.append(result)
+        return data
+    else:
+        raise HTTPException(404, f"Couldn't find menu with name: {name}'")
+
 
 @app.get("reservation/by-table/{table}")
 def get_reservation_by_table(table: int):
-    pass
+    list_result = list(collection.find({"table_number": table}, {"_id": 0}))
+    if len(list_result) != 0:
+        data = []
+        for result in list_result:
+            data.append(result)
+        return data
+    else:
+        raise HTTPException(404, f"Couldn't find menu with name: {table}'")
+
 
 @app.post("/reservation")
-def reserve(reservation : Reservation):
-    pass
+def reserve(reservation: Reservation):
+    find = collection.find_one({"time": reservation.time}, {"table_number": reservation.table_number})
+    if find != None:
+        return {
+            "result": "Failed"
+        }
+    else:
+        if reservation.table_number > 12:
+            return {
+                "result": "Failed"
+            }
+        else:
+            res_encode = jsonable_encoder(reservation)
+            collection.insert_one(res_encode)
+            return {
+                "result": "done"
+            }
+
 
 @app.put("/reservation/update/")
 def update_reservation(reservation: Reservation):
-    pass
+    find = collection.find_one({"time": reservation.time}, {"table_number": reservation.table_number})
+    if find != None:
+        return {
+            "result": "Failed"
+        }
+    elif find == None:
+        return {
+            "result": "Failed"
+        }
+    else:
+        new_value = {
+            "$set": {"name": reservation.name, "time": reservation.time, "table_number": reservation.table_number}}
+        collection.update_one(collection.find(), new_value)
+        res_encode = jsonable_encoder(reservation)
+        collection.update_one(res_encode)
+        return {
+            "result": "done"
+        }
+
 
 @app.delete("/reservation/delete/{name}/{table_number}")
-def cancel_reservation(name: str, table_number : int):
-    pass
-
+def cancel_reservation(name: str, table_number: int):
+    find = collection.find_one({"name": name, "table_number": table_number})
+    if find == None:
+        return {
+            "result": "Failed"
+        }
+    else:
+        collection.delete_one()
+        return {
+            "result": "done"
+        }
